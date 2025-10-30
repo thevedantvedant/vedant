2c542a6d7cbeac5719381cf1db09dcfd8004a208
Vedant <itsmevedants@gmail.com>
2025-10-30 16:36:35 +0530

    Create App component with layout structure

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import Navbar from "./components/Navbar";
+import Hero from "./components/Hero";
+import About from "./components/About";
+import Projects from "./components/Projects";
+import Contact from "./components/Contact";
+
+function App() {
+  return (
+    <div className="font-sans bg-dark text-white">
+      <Navbar />
+      <Hero />
+      <About />
+      <Projects />
+      <Contact />
+    </div>
+  );
+}
+
+export default App;
